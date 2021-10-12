92df86d0606b45e40f6d87ee3eabcfdb173b3afb
abdoulkarim <abdoulkarimfoly1996@gmail.com>
2021-10-12 13:09:59 +0000

    j'ai ajouter la page a propos

---
+++ propos.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="karim.css">
+    <title>A Propos de nous</title>
+</head>
+<body>
+    <header id="showcase">
+        <h1>A PROPOS DE GROUPE FSOCIETY P4</h1>
+        <p>Nous somme une equipe composer de 5 apprenants dont les noms sont les suivants</p>
+        <p>Abdoul karim DIALLO</p>
+        <p>Malick KEBE</p>
+        <p>DAME Leye</p>
+        <p>Ramata KA</p>
+        <p>Rokhaya GUEYE</p>
+        <a href="https://senegal.simplon.co/" class="button">En savoir plus sur SIMPLON.co SENEGAL</a>
+    </header>
+</body>
+</html>
