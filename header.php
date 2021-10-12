1790eb34a615cf47f8c34b014472ad79c6fb397e
honorablecon <honorablecon@gmail.com>
2021-10-12 12:47:11 +0000

    initial commit

---
+++ header.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="./images/logo.png" alt="" width="50" height="50" class="d-inline-block align-text-top">
+            Fsociety
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link" href="contact.php">Contact</a>
+            <a class="nav-link" href="apropos.php">A propos</a>
+            <a class="nav-link" href="incription.php">Sign up</a>
+            <a class="nav-link" href="login.php">Login</a>
+        </div>
+        </div>
+    </div>
+</nav>
